2a8262afcbcd2bf9860899eda3f7fbd80cf302be
Johan Smarius <jsmarius@hotmail.com>
2021-11-29 20:57:40 +0100

    Add project files.

---
+++ ImageUpload/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageUpload.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public byte[] Picture { get; set; }
+}
